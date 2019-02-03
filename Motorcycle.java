21842b2e4f4d2129000ee869cbe35a861c626d9e
Nayay <snayay@gmail.com>
2019-02-02 19:07:16 -0500

    Wrong Path Remove Files

--- Motorcycle.java
+++
@@ -1,5 +0,0 @@
-package com.payroll;
-
-public class Motorcycle {
-
-}
--- PartTime.java
+++
@@ -1,7 +0,0 @@
-package com.payroll;
-
-public abstract class PartTime {
-	private double rate;
-	private float hoursWorked;
-
-}
