34e28867ddd7b1adde81f31d51b633f19b0737cf
prabhjot143 <prabhjotpannu78@gmail.com>
2019-02-04 14:36:27 -0500

    Fixed Base and intern

--- src/com/payroll/FixedBasedPartTime.java
+++ src/com/payroll/FixedBasedPartTime.java
@@ -1,19 +1,45 @@
 package com.payroll;
 
-public class FixedBasedPartTime {
+	public class FixedBasedPartTime extends PartTime {
 
-	private double fixedAmount;
-	
-	public double getFixedAmount() {
-		return fixedAmount;
-	}
-
-	public void setFixedAmount(double fixedAmount) {
-		this.fixedAmount = fixedAmount;
-	}
-
-	public void calcEarnings(){
+		private double fixedAmount;
 		
-		//(rate*hoursWorked)+FixedAmount
-	}
+		FixedBasedPartTime(String name, int age, float rate, float hoursWorked, double fixedAmount, Vehicle vehicle) {
+	        super(name, age, rate, hoursWorked);
+	        this.fixedAmount=fixedAmount;
+	        
+	    }
+		
+		public double getFixedAmount() {
+			return fixedAmount;
+		}
+
+		public void setFixedAmount(double fixedAmount) {
+			this.fixedAmount = fixedAmount;
+		}
+
+		public double calcEarnings(){
+			 return (getRate() * getHoursWorked()) + getFixedAmount();
+			
+			//(rate*hoursWorked)+FixedAmount
+		}
+		
+		@Override
+	    public String printMyData() {
+	        return super.printMyData();
+	    }
+		 @Override
+		    public String toString() {
+		        StringBuilder stringBuilder = new StringBuilder();
+		        stringBuilder.append("Name: " + getName() + "\n" + "Age: " + getAge() + "\n");
+		        
+		        stringBuilder.append("\nEmployee is PartTime/ Fixed Amount")
+		                .append("\n -Rate: " + getRate())
+		                .append("\n -Hours Worked: " + getHoursWorked())
+		                .append("\n -Fixed amount: " + getFixedAmount())
+		                .append("\n -Earnings: " + calcEarnings())
+		                .append("(" + getHoursWorked() * getRate() + " + " + getFixedAmount()+")");
+		        return String.valueOf(stringBuilder);
+		    }
+
 }
--- src/com/payroll/Intern.java
+++ src/com/payroll/Intern.java
@@ -1,15 +1,53 @@
 package com.payroll;
 
-public class Intern {
-	
-	private String schoolName;
+	public class Intern extends Employee{
+		private String schoolName;
+		 private double earnings;
+		
 
-	public String getSchoolName() {
-		return schoolName;
-	}
 
-	public void setSchoolName(String schoolName) {
-		this.schoolName = schoolName;
-	}
+
+		public Intern(String name, int age, String schoolName) {
+			super(name, age);
+	        this.schoolName=schoolName;
+	        
+			// TODO Auto-generated constructor stub
+		}
+
+		
+		public double getEarnings() {
+			return earnings;
+		}
+		public void setEarnings(double earnings) {
+			this.earnings = earnings;
+		}
+		public String getSchoolName() {
+			return schoolName;
+		}
+
+		public void setSchoolName(String schoolName) {
+			this.schoolName = schoolName;
+		}
+		
+		
+		
+		   public double calcEarnings() {
+		        return earnings;
+		    }
+		@Override
+	    public String printMyData() {
+	        return "Employee Name "+getName()+"Age "+getAge()+"Intern School Name" +schoolName;
+	    }
+
+	    @Override
+	    public String toString() {
+	        StringBuilder stringBuilder = new StringBuilder();
+	        stringBuilder.append("Name: " + getName() + "\n" + "Age: " + getAge() + "\n");
+	        stringBuilder.append("\nEmployee is Intern")
+	                .append("\n -School name: "+getSchoolName())
+	                .append("\n -Earnings: "+calcEarnings());
+	        return String.valueOf(stringBuilder);
+	    }
+
 
 }
