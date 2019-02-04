ccdb50cbbcf019229b191ae64de367e6da3ca878
Nayay <snayay@gmail.com>
2019-02-04 14:26:58 -0500

    Payroll System ArrayList

--- src/com/payroll/PayrollSystem.java
+++ src/com/payroll/PayrollSystem.java
@@ -1,11 +1,18 @@
 package com.payroll;
 
+import java.util.ArrayList;
+
 public class PayrollSystem {
 	
 	
     public static void main(String []args){
     	
-        
+    	 CommissionBasedPartTime commissionBasedPartTime= new CommissionBasedPartTime("Nayay", 24, 20.0f,5.0f, 10);
+   	  ArrayList<Employee> employeeArrayList= new ArrayList<>();
+         employeeArrayList.add(commissionBasedPartTime);
+         for (Employee e: employeeArrayList) {
+       	  System.out.println(e);
+         }
 
     }
 
