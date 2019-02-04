6b281affe5f3032a19a47f3e19a0cdc6d63d8d9d
Nayay <snayay@gmail.com>
2019-02-04 18:57:34 -0500

    Project Draft1

--- src/com/payroll/PayrollSystem.java
+++ src/com/payroll/PayrollSystem.java
@@ -9,14 +9,16 @@ public class PayrollSystem {
     	 
     	
     	 CommissionBasedPartTime commissionBasedPartTime= new CommissionBasedPartTime("Nayay", 24, 40.0f,10.0f, 40,new Car("Hyundai","Black","JOT",3000.0,"4"));
-    	// FixedBasedPartTime fixedBased=new FixedBasedPartTime("Jot",22,20.0f,9.0f,300,null);
-    	// Intern intern=new Intern("Aman",21,"Lambton College",null);
-    	 //FullTime fullTime=new FullTime("Nayay",26,2200,1000,new Car("Hyundai","Black","JOT",3000.0,"4"));
+    	 
+    	 FixedBasedPartTime fixedBased=new FixedBasedPartTime("Jot",22,20.0f,9.0f,300,null);
+    	 Intern intern=new Intern("Aman",21,"Lambton College",null);
+    	 FullTime fullTime=new FullTime("Nayay",26,2200,1000,new Car("Hyundai","Black","JOT",3000.0,"4"));
     	 ArrayList<Employee> employeeArrayList= new ArrayList<>();
-         employeeArrayList.add(commissionBasedPartTime);
-        // employeeArrayList.add(intern);
-//         employeeArrayList.add(fixedBased);
-//         employeeArrayList.add(fullTime);
+       
+    	 employeeArrayList.add(commissionBasedPartTime);
+    	 	employeeArrayList.add(intern);
+    	 	employeeArrayList.add(fixedBased);
+    	 	employeeArrayList.add(fullTime);
          for (Employee e: employeeArrayList) {
           System.out.println("----------------------------------------------------------------------------- \n");
        	  System.out.println(e);
