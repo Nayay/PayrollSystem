b0e3f608c15d3da985655defbac22a462ea3330a
Nayay <snayay@gmail.com>
2019-02-04 17:20:34 -0500

    Commisssion based is corrected

--- src/com/payroll/CommissionBasedPartTime.java
+++ src/com/payroll/CommissionBasedPartTime.java
@@ -1,53 +1,63 @@
 package com.payroll;
 
 public class CommissionBasedPartTime extends PartTime {
-	
-	private double commissionPerc; 
-	
+	private double commissionPerc;
+	private Vehicle vehicle;
 
-    public CommissionBasedPartTime(String name, int age, float rate, float hoursWorked, double commissionPerc) {
-        super(name, age, rate, hoursWorked);
-        this.commissionPerc=commissionPerc;
-    }
 
-	
-	public double getCommissionPerc() {
+	public CommissionBasedPartTime(String name, int age, float rate, float hoursWorked, double commissionPerc) {
+		super(name, age, rate, hoursWorked);
+		setcommissionPerc(commissionPerc);
+	}
+
+	@Override
+	public double calcEarnings() {
+		return (getRate() * getHoursWorked()) + calculateComission();
+	}
+
+	@Override
+	public String printMyData() {
+		return super.printMyData();
+	}
+
+	public double calculateComission(){
+		double calculatedComission=0;
+
+		if (commissionPerc!=0){
+			double totalAmount=getRate()*getHoursWorked();
+			calculatedComission=(getcommissionPerc()/100)*totalAmount;
+		}
+		return calculatedComission;
+	}
+
+
+	public double getcommissionPerc() {
 		return commissionPerc;
 	}
-	public void setCommissionPerc(double commissionPerc) {
-		this.commissionPerc = commissionPerc;
+
+	public void setcommissionPerc(double commissionPerc) {
+		if (commissionPerc > 0.0) {
+			this.commissionPerc = commissionPerc;
+		} else {
+			throw new IllegalArgumentException("Commission rate must be >0");
+		}
 	}
-	
-	
-	public double calcEarnings(){
-        double calculatedComission=0;
 
-        if (commissionPerc!=0){
-            double totalAmount=getRate()*getHoursWorked();
-            calculatedComission=(getCommissionPerc()/100)*totalAmount;
-        }
-        return calculatedComission;
-    }
-	
-	  @Override
-	    public String printMyData() {
-	        return this.toString();
-	    }
-	  @Override
-	    public String toString() {
-	        StringBuilder stringBuilder = new StringBuilder();
-	        stringBuilder.append("Name: " + getName() + "\n" + "Age: " + getAge() + "\n");
-	        stringBuilder.append("Employee is PartTime\\Comissioned")
-	                .append("\n -Rate: " + getRate())
-	                .append("\n -Hours Worked: " + getHoursWorked())
-	                .append("\n -Comission: " + commissionPerc)
-	                .append("\n -Earnings: " + calcEarnings())
-	                .append("\n (" + getHoursWorked() * getRate() + " + " + getCommissionPerc() + "% of " + getHoursWorked() * getRate() + ")");
-	        return String.valueOf(stringBuilder);
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Name: " + getName() + "\n" + "Year of Birth: " + getAge() + "\n");
+		stringBuilder.append("Employee is PartTime/ Comissioned")
+				.append("\n -Rate: " + getRate())
+				.append("\n -Hours Worked: " + getHoursWorked())
+				.append("\n -Comission: " + commissionPerc)
+				.append("\n -Earnings: " + calcEarnings())
+				.append("(" + getHoursWorked() * getRate() + " + " + getcommissionPerc() + "% of " + getHoursWorked() * getRate() + ")");
+		return String.valueOf(stringBuilder);
 
-	    }
-	    
-	    
+	}
+
+
 
 
 }
--- src/com/payroll/PayrollSystem.java
+++ src/com/payroll/PayrollSystem.java
@@ -6,17 +6,18 @@ public class PayrollSystem {
 	
 	
     public static void main(String []args){
-    	
-    	 CommissionBasedPartTime commissionBasedPartTime= new CommissionBasedPartTime("Nayay", 24, 20.0f,5.0f, 10);
+    	 
+    	 CommissionBasedPartTime commissionBasedPartTime= new CommissionBasedPartTime("Nayay", 24, 40.0f,10.0f, 40);
     	 FixedBasedPartTime fixedBased=new FixedBasedPartTime("Jot",22,20.0f,9.0f,300);
     	 Intern intern=new Intern("Aman",21,"Lambton College");
     	 FullTime fullTime=new FullTime("Nayay",26,2200,1000);
     	 ArrayList<Employee> employeeArrayList= new ArrayList<>();
-        // employeeArrayList.add(commissionBasedPartTime);
+         employeeArrayList.add(commissionBasedPartTime);
          employeeArrayList.add(intern);
          employeeArrayList.add(fixedBased);
          employeeArrayList.add(fullTime);
          for (Employee e: employeeArrayList) {
+          System.out.println("----------------------------------------------------------------------------- \n");
        	  System.out.println(e);
          }
 
