ab230d4feaff783c7d54efa4ccafd1085360dc50
prabhjot143 <prabhjotpannu78@gmail.com>
2019-02-04 18:40:00 -0500

    Vehicle relationship with commission based

--- src/com/payroll/CommissionBasedPartTime.java
+++ src/com/payroll/CommissionBasedPartTime.java
@@ -5,6 +5,22 @@ public class CommissionBasedPartTime extends PartTime {
 	private Vehicle vehicle;
 
 
+	public double getCommissionPerc() {
+		return commissionPerc;
+	}
+
+	public void setCommissionPerc(double commissionPerc) {
+		this.commissionPerc = commissionPerc;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
 	public CommissionBasedPartTime(String name, int age, float rate, float hoursWorked, double commissionPerc,Vehicle vehicle) {
 		super(name, age, rate, hoursWorked, vehicle);
 		setcommissionPerc(commissionPerc);
@@ -42,7 +58,30 @@ public class CommissionBasedPartTime extends PartTime {
 			throw new IllegalArgumentException("Commission rate must be >0");
 		}
 	}
+	
+	
+	//Vehicle block
 
+    private void checkVehicle(StringBuilder stringBuilder) {
+        if (vehicle == null) {
+            stringBuilder.append("Employee has no vehicle registerd");
+        } else if (vehicle instanceof Car) {
+            stringBuilder.append("Employee has a Car\n")
+                    .append(" -Make: ").append(getVehicle().getMake())
+                    .append("\n -Plate: ").append(getVehicle().getPlate())
+                    .append("\n -Color: ").append(getVehicle().getColor())
+                    .append("\n -Distance Travelled: ").append(((Car)getVehicle()).getDistanceTravelled())
+                    .append("\n -Capacity: ").append(((Car)getVehicle()).getCapacity());
+                    
+        	} else if (vehicle instanceof Motorcycle) {
+            stringBuilder.append("Employee has a MotorCycle\n")
+                    .append(" -Make: ").append(getVehicle().getMake())
+                    .append("\n -Plate: ").append(getVehicle().getPlate())
+                    .append("\n -Color: ").append(getVehicle().getColor())
+                    .append("\n -Distance Travelled: ").append(((Motorcycle)getVehicle()).getDistanceTravelled())
+                    ;
+        }
+    }
 	@Override
 	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder();
@@ -52,7 +91,8 @@ public class CommissionBasedPartTime extends PartTime {
 				.append("\n -Hours Worked: " + getHoursWorked())
 				.append("\n -Comission: " + commissionPerc)
 				.append("\n -Earnings: " + calcEarnings())
-				.append("(" + getHoursWorked() * getRate() + " + " + getcommissionPerc() + "% of " + getHoursWorked() * getRate() + ")");
+				.append("(" + getHoursWorked() * getRate() + " + " + getcommissionPerc() + "% of " + getHoursWorked() * getRate() + ")"+"\n");
+		checkVehicle(stringBuilder);
 		return String.valueOf(stringBuilder);
 
 	}
