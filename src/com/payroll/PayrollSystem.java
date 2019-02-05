1aa685df787f5b18e7554cb10aac8e6dc61b4d55
Nayay <snayay@gmail.com>
2019-02-04 19:37:40 -0500

    Payroll Data

--- src/com/payroll/PayrollSystem.java
+++ src/com/payroll/PayrollSystem.java
@@ -7,9 +7,9 @@ public class PayrollSystem {
 	
     public static void main(String []args){
     	 
-    	Car car= new Car("Hyundai","Black","Nayay",3000.0,"4");
+    	Motorcycle bike= new Motorcycle("Ninja","Black","JOT",3000.0);
         Car car1= new Car("Wrangler","Black","JOT",43000.0,"6");
-        CommissionBasedPartTime commissionBasedPartTime= new CommissionBasedPartTime("Nayay", 24, 40.0f,10.0f, 40,car);   
+        CommissionBasedPartTime commissionBasedPartTime= new CommissionBasedPartTime("Nayay", 24, 40.0f,10.0f, 40,bike);   
         FixedBasedPartTime fixedBased=new FixedBasedPartTime("Sandeep Singh",22,20.0f,9.0f,300,null);
         Intern intern=new Intern("Prabhjot Pannu",21,"Lambton College",car1);
         FullTime fullTime=new FullTime("Aman Mann",24,7200,1000,new Car("Range rover","Black","JOT",3000.0,"5"));
